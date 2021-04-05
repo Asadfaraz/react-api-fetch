76daadd058cf1841d158ce9a9ee2814df8650cf7
Asad Faraz <asadfaraz44@gmail.com>
2021-04-05 09:53:17 +0500

    fetch data

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Fetching data from API in React js
+
+## Details
+
+I used fetch function of javascript and async await to fetch users from github users api (https://api.github.com/users).
+Then I used useEffect and useState hooks to use the data and display it.
--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const url = "https://api.github.com/users";
+
+  const [users, setusers] = useState([]);
+
+  const getUsers = async () => {
+    const response = await fetch(url);
+    const users = await response.json();
+    // console.log(users);
+    setusers(users);
+  };
+
+  // UseEffect when users api call
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <div className="App">
       <h1>Fetching API's</h1>
+      {/* getting all the users */}
+      <ul>
+        {users.map((user) => {
+          const { id, html_url, login, avatar_url } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <section>
+                <h3>{login}</h3>
+                <h3>{html_url}</h3>
+              </section>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
